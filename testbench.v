c0e22ee86a8515fcbf21d6a651c4c867a6ef7764
José Villalobos <jose.villalobos.j@utec.edu.pe>
2026-06-10 12:36:54 -0500

    single_cycle base

---
+++ testbench.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ns
+
+module testbench;
+  reg          clk;
+  reg          reset;
+  wire [31:0]  WriteData;
+  wire [31:0]  Adr;
+  wire         MemWrite;
+  
+  // instantiate device to be tested
+  top dut(
+    .clk(clk), 
+    .reset(reset), 
+    .WriteData(WriteData), 
+    .Adr(Adr), 
+    .MemWrite(MemWrite)    
+  );
+
+  // initialize test
+  initial begin
+    reset = 1; # 22;
+    reset = 0;
+  end
+
+  // generate clock to sequence tests
+  always begin
+    clk = 1;
+    # 5; clk = 0; # 5;
+  end
+
+  // check results
+  always @(negedge clk) begin
+    if(MemWrite) begin
+      if(Adr === 104 & WriteData === 25) begin
+        $display("Simulation succeeded");
+        $stop;
+      end else if (Adr !== 96) begin
+        $display("Simulation failed");
+        $stop;
+      end
+    end
+  end
+endmodule
