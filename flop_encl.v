b8c80c39473ef9a063bf39088a9eb6e19b3fbfca
José Villalobos <jose.villalobos.j@utec.edu.pe>
2026-06-16 16:24:44 -0500

    creado flop_encl.v : flip flop con opciones de clear y enable

---
+++ flop_encl.v
@@ -0,0 +1,14 @@
+module enable_flipflop (input  clk, reset, enable, clear
+               input  [WIDTH-1:0] d, 
+               output [WIDTH-1:0] q);
+
+  parameter WIDTH = 8;
+
+  reg [WIDTH-1:0] q; 
+
+  always @(posedge clk) begin 
+    if (reset) q <= 0;
+    else if (clear) q <= 0;
+    else if (enable) q <= d;
+  end
+endmodule
