95ef0cfa5e69ee9d343dad81e52390ad4f23fbfd
José Villalobos <jose.villalobos.j@utec.edu.pe>
2026-06-23 22:31:34 -0500

    instrucciones y testbench para probar instrucciones comprimidas

--- testbench.v
+++ testbench.v
@@ -7,7 +7,6 @@ module testbench;
   wire [31:0]  DataAdr;
   wire         MemWrite;
   
-  
   top dut(
     .clk(clk), 
     .reset(reset), 
@@ -21,7 +20,6 @@ module testbench;
     reset = 0;
   end
 
-
   always begin
     clk = 1;
     # 5; clk = 0; # 5;
@@ -31,5 +29,17 @@ module testbench;
     #2000;
     $display("Finishing...");
     $finish;
+  end
+
+  always @(negedge clk) begin
+    if(MemWrite) begin
+      if(DataAdr === 32'd8 & WriteData === 32'd40) begin
+        $display("Simulation succeeded");
+        $stop;
+      end else begin
+        $display("Simulation failed");
+        $stop;
+      end
+    end
   end
 endmodule
